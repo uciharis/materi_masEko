6a81cae660621867591a90bea5a5a2d17d99ea51
uciharis <harisnop@gmail.com>
2023-01-20 18:44:59 +0700

    materi arrow funct pada objek

--- 45_arrow_funct.js
+++ 45_arrow_funct.js
@@ -1,5 +1,24 @@
 //arrow function di objek
 //arrow funct bisa digunakan sbg Object Method
-//arrow funct tdk bs digunakan utk akses argumen objek, funct generator
+//arrow funct tdk bs digunakan utk akses argumen objek, funct generator,
 //- kata kunci this dan kata kunci super (dibahas di OOP)
 //pastikan arrow funct hanya memang digunakan ketika tidak butuh fitur diatas
+const person = {
+    name: "eko",
+    sayHello: (name) => {
+        console.info(`hello ${name}`)
+    }
+};
+person.sayHello("budi"); //output hello budi di anonimous function
+// bandingkan dengan ini sblm menggunakan arrow function d objek
+const parson = {
+    nama: "ekoooo",
+    sayHallo: function(nama){
+        console.info(`hellauuu ${nama}`);
+    }
+};
+parson.sayHallo("kuntjorooooo"); //output hellauuu kuntjoro 
+// -alias sama dengan output objek diatas tanpa arrow function
+
+//kata kunci this pada arrow function mengacu pada window
+// sedang this pada anonimous function mengacu pada parent nya
